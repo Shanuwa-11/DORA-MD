6084146eb2d3dbae5b33030228c001734ac8415b
DORA-MD-2 <doraisi487@gmail.com>
2025-04-07 22:43:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "Xi53QarR#oBG1s8NluR1MfUBb46Ith_dfbxuDKUQzCOXF3eLiX6Y",
   OWNER_NUM: process.env.OWNER_NUM || "94763007898",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/GQBGW0WB/8181.jpg",
