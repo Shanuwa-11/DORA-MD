6bfe061be7b0c36d9aeaac5de427611337a28616
DORA-MD-2 <doraisi487@gmail.com>
2025-04-08 19:05:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "2SBwHTqS#yNwb9927nx4PxlZ53KRygrVJQyaDJYAEIu-Zizx32g0",
+  SESSION_ID: process.env.SESSION_ID || "Fzk0QQpS#IzVRRkIUknwGXbkq2CEiq0UhEG62qs1cA3Bi9ooJr2g",
   OWNER_NUM: process.env.OWNER_NUM || "94763007898",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/GQBGW0WB/8181.jpg",
