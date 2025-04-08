009a7d5db333190216a18b3efa9163a0e7742745
DORA-MD-2 <doraisi487@gmail.com>
2025-04-09 00:16:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Fzk0QQpS#IzVRRkIUknwGXbkq2CEiq0UhEG62qs1cA3Bi9ooJr2g",
   OWNER_NUM: process.env.OWNER_NUM || "94763007898",
   PREFIX: process.env.PREFIX || ".",
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/GQBGW0WB/8181.jpg",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/i5fwv5.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n*🍁 CREATE BY DORA 🍁*\n\n*watsapp channel link* - https://whatsapp.com/channel/0029Vag1WQFJf05dF0pQeU3u",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   MODE: process.env.MODE || "public",
